82848a9932a90e126f0e69ff8d271b0ccbdac978
aurimrv <aurimrv@yahoo.com.br>
2021-05-16 22:31:37 -0300

    Refactoring to deal with two roman symbols

--- src/roman2dec.py
+++ src/roman2dec.py
@@ -1,5 +1,9 @@
 class Roman2Decimal:
-	table = {'I':1, 'II':2, 'V': 5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+	table = {'I':1, 'V': 5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 
 	def converte(self,roman):
-		return self.table.get(roman)
+		sumup = 0
+		for symbol in roman:
+			sumup = sumup + self.table.get(symbol)
+
+		return sumup
